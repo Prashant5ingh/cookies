18d00b81742e466d24ed730686707d37fda2d4dc
Prashant5ingh <primeprashant38881@gmail.com>
2021-12-31 13:43:10 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cookies';
+  constructor(private cookie: CookieService){
+
+  }
+  login(){
+    console.log("Login Method is Called..")
+// once the response is recieved we are going to save those info in our cookie
+this.cookie.set("UserId","1237");
+this.cookie.set("Usertype","Admin");
+
+//Getting a cookie
+alert("User with id ("+ this.cookie.get("UserId")+ ") successfully logged in.")
+}
 }
