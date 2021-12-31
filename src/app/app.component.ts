85dda35f1ceef777d2f85f5ed9568aa6e86023e3
Prashant5ingh <primeprashant38881@gmail.com>
2021-12-31 12:30:53 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { ExampleService } from './example.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+ 
+  
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string;
+  constructor(private _exampleService: ExampleService){  // In arguments dependency injection ocuurs
+   
+  }
+  ngOnInit(){
+    this.title=this._exampleService.somemethod(); 
+  }
+    // indicates that angular is done creating the components in browser and start executing imediately
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleService } from './example.service';
+import { MainComponent } from './IOT/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ExampleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
